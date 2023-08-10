6f931a65f6501e666b6d71d7ee4e4f4b2af392ab
Maxim <38497831+StableBig@users.noreply.github.com>
2023-08-10 19:13:55 +0200

    Add docstrings to seller.py

--- seller.py
+++ seller.py
@@ -12,7 +12,27 @@ logger = logging.getLogger(__file__)
 
 
 def get_product_list(last_id, client_id, seller_token):
-    """Получить список товаров магазина озон"""
+    """
+    Retrieve a list of products from the Ozon seller's store.
+
+    This function fetches a list of products based on the given `last_id`, using the provided `client_id` and `seller_token`
+    to authenticate against the Ozon seller API.
+
+    Args:
+        last_id (int): The last product ID to begin retrieval from.
+        client_id (str): The client ID for API access.
+        seller_token (str): The API token for the seller's store.
+
+    Returns:
+        dict: The result containing the products list and other related data.
+
+    Examples:
+        >>> get_product_list(0, "YOUR_CLIENT_ID", "YOUR_SELLER_TOKEN")
+        {"items": [{"product_id": 123, "name": "Watch", ...}], ...}
+
+    Errors:
+        HTTPError: Raises an exception if the request to the API fails.
+    """
     url = "https://api-seller.ozon.ru/v2/product/list"
     headers = {
         "Client-Id": client_id,
@@ -32,7 +52,24 @@ def get_product_list(last_id, client_id, seller_token):
 
 
 def get_offer_ids(client_id, seller_token):
-    """Получить артикулы товаров магазина озон"""
+    """
+    Retrieves a list of offer IDs from the server.
+
+    Args:
+        client_id (str): The client's ID.
+        seller_token (str): The seller's authentication token.
+
+    Returns:
+        List[str]: A list of strings, each representing an offer ID.
+
+    Examples:
+        # Assuming an initialized server with available offer IDs.
+        >>> get_offer_ids("client_123", "token_456")
+        ["123", "456", "789", ...]
+
+    Errors:
+        Invalid client_id or seller_token, or a server-side error may result in retrieval failure.
+    """
     last_id = ""
     product_list = []
     while True:
@@ -49,7 +86,24 @@ def get_offer_ids(client_id, seller_token):
 
 
 def update_price(prices: list, client_id, seller_token):
-    """Обновить цены товаров"""
+    """
+    Updates prices on the server based on the provided list of prices.
+
+    Args:
+        prices (List[Dict[str, str]]): A list of dictionaries, each representing a price entry.
+        client_id (str): The client's ID.
+        seller_token (str): The seller's authentication token.
+
+    Returns:
+        None: This function returns nothing, but it updates the prices on the server side.
+
+    Examples:
+        >>> update_price([{"offer_id": "123", "price": "5990"}], "client_123", "token_456")
+        # No return value, but the price for offer_id "123" has been updated to "5990" on the server.
+
+    Errors:
+        Invalid client_id or seller_token, or a server-side error may result in update failure.
+    """
     url = "https://api-seller.ozon.ru/v1/product/import/prices"
     headers = {
         "Client-Id": client_id,
@@ -62,7 +116,24 @@ def update_price(prices: list, client_id, seller_token):
 
 
 def update_stocks(stocks: list, client_id, seller_token):
-    """Обновить остатки"""
+    """
+    Updates stock quantities on the server based on the provided list of stocks.
+
+    Args:
+        stocks (List[Dict[str, Union[str, int]]]): A list of dictionaries, each representing a stock entry.
+        client_id (str): The client's ID.
+        seller_token (str): The seller's authentication token.
+
+    Returns:
+        None: This function returns nothing, but it updates the stocks on the server side.
+
+    Examples:
+        >>> update_stocks([{"offer_id": "123", "stock": 5}], "client_123", "token_456")
+        # No return value, but the stock for offer_id "123" has been updated to 5 on the server.
+
+    Errors:
+        Invalid client_id or seller_token, or a server-side error may result in update failure.
+    """
     url = "https://api-seller.ozon.ru/v1/product/import/stocks"
     headers = {
         "Client-Id": client_id,
@@ -75,7 +146,20 @@ def update_stocks(stocks: list, client_id, seller_token):
 
 
 def download_stock():
-    """Скачать файл ostatki с сайта casio"""
+    """
+    Downloads a list of watch remnants (stock) from the CASIO website.
+
+    Returns:
+        List[Dict[str, Any]]: A list of dictionaries, each representing a watch remnant.
+
+    Examples:
+        # Assuming an initialized server with available data.
+        >>> download_stock()
+        [{"Код": "123", "Количество": "5", ...}, ...]
+
+    Errors:
+        Server connection issues or unavailability may result in download errors.
+    """
     # Скачать остатки с сайта
     casio_url = "https://timeworld.ru/upload/files/ostatki.zip"
     session = requests.Session()
@@ -96,6 +180,25 @@ def download_stock():
 
 
 def create_stocks(watch_remnants, offer_ids):
+    """
+    Generates a list of stocks based on available watch remnants and offer IDs.
+
+    Args:
+        watch_remnants (List[Dict[str, Any]]): A list of watches with their details.
+        offer_ids (List[str]): A list of available offer IDs.
+
+    Returns:
+        List[Dict[str, Union[str, int]]]: A list of dictionaries, each representing a stock entry.
+
+    Examples:
+        >>> create_stocks([{"Код": "123", "Количество": "5"}], ["123"])
+        [{"offer_id": "123", "stock": 5}]
+
+    Errors:
+        Unexpected data format may yield incorrect stock entries.
+        >>> create_stocks([{"WrongKey": "123"}], ["123"])
+        [{"offer_id": "123", "stock": 0}]
+    """
     # Уберем то, что не загружено в seller
     stocks = []
     for watch in watch_remnants:
@@ -116,6 +219,23 @@ def create_stocks(watch_remnants, offer_ids):
 
 
 def create_prices(watch_remnants, offer_ids):
+    """
+    Generates a list of prices based on available watch remnants and offer IDs.
+
+    Args:
+        watch_remnants (List[Dict[str, Any]]): A list of watches with their details.
+        offer_ids (List[str]): A list of available offer IDs.
+
+    Returns:
+        List[Dict[str, Any]]: A list of dictionaries, each representing a price entry.
+
+    Examples:
+        >>> create_prices([{"Код": "123", "Цена": "5'990.00 руб."}], ["123"])
+        [{"auto_action_enabled": "UNKNOWN", "currency_code": "RUB", "offer_id": "123", "old_price": "0", "price": "5990"}]
+
+    Errors:
+        Unexpected data format may yield incorrect price entries.
+    """
     prices = []
     for watch in watch_remnants:
         if str(watch.get("Код")) in offer_ids:
@@ -156,12 +276,31 @@ def price_conversion(price: str) -> str:
 
 
 def divide(lst: list, n: int):
-    """Разделить список lst на части по n элементов"""
+    """
+    Splits a list into chunks of size 'n'.
+
+    Args:
+        lst (List[Any]): A list to be divided.
+        n (int): The size of each chunk.
+
+    Returns:
+        Generator[List[Any], None, None]: A generator yielding lists with a maximum size of 'n'.
+
+    Examples:
+        >>> list(divide([1, 2, 3, 4], 2))
+        [[1, 2], [3, 4]]
+
+    Errors:
+        Negative or zero 'n' will raise a ValueError.
+        >>> list(divide([1, 2, 3, 4], -1))
+        ValueError: ...
+    """
     for i in range(0, len(lst), n):
         yield lst[i : i + n]
 
 
 async def upload_prices(watch_remnants, client_id, seller_token):
+    """Asynchronously uploads prices based on available watch remnants to a server."""
     offer_ids = get_offer_ids(client_id, seller_token)
     prices = create_prices(watch_remnants, offer_ids)
     for some_price in list(divide(prices, 1000)):
@@ -170,6 +309,7 @@ async def upload_prices(watch_remnants, client_id, seller_token):
 
 
 async def upload_stocks(watch_remnants, client_id, seller_token):
+    """Asynchronously uploads stocks based on available watch remnants to a server."""
     offer_ids = get_offer_ids(client_id, seller_token)
     stocks = create_stocks(watch_remnants, offer_ids)
     for some_stock in list(divide(stocks, 100)):
